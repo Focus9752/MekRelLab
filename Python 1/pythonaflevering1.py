378f65a972c17a2e5601a4a312edcfd3900515cc
Focus9752 <50440281+Focus9752@users.noreply.github.com>
2023-09-14 15:07:03 +0200

    Create pythonaflevering1.py

---
+++ Python 1/pythonaflevering1.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# Masse/svingingstid
+#
+# Massen af loddet i gram
+m_lod = np.array([10, 20, 50, 100, 200, 500, 1000])
+# Svingingstiden for 5 svinginger
+Tm1 = np.array([])
+
+# Snorlængde/sviningstid
+#
+# Længde af snor i cm
+l_snor = np.array([51.5, 51.5, 47.5, 47.5, 35.1, 35.1, 25.1, 25.1, 15.7, 15.7, 3.4, 3.4])
+# Svingingstiden for 5 svininger
+Tl = np.array([8.40, 8.33, 8.31, 8.00, 7.13, 7.13, 6.10, 5.95, 4.93, 5.13, 3.15, 3.15])
+
+
+
